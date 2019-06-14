e2182cd49877940d8a4d9aa80bd07e6ea71f2d24
luotao <luotaoyeah@gmail.com>
2019-06-14 10:25:56 +0800

    feat(doc): 01_01_06: variables

--- src/doc/01_samples_tutorials/01_language_samples/03-hello-world/01_01_03.dart
+++ src/doc/01_samples_tutorials/01_language_samples/03-hello-world/01_01_03.dart
@@ -1,3 +1,10 @@
+/*
+ * https://dart.dev/samples#hello-world
+ */
+
+/**
+ * main() 方法是整个应用的入口
+ */
 void main() {
   print('hello world');
 }
---
+++ src/doc/01_samples_tutorials/01_language_samples/05-variables/01_01_05.dart
@@ -0,0 +1,20 @@
+/*
+ * https://dart.dev/samples#variables
+ */
+
+void main() {
+  /*
+   * 使用关键字 var 来声明一个变量,
+   * 因为有 type inference, 可以不用显式声明变量类型
+   */
+
+  var str01 = "FOO";
+  var num01 = 99;
+  var arr01 = ["A", "B", "C"];
+  var obj01 = {"x": 'X', "y": 'Y'};
+
+  print(str01);
+  print(num01);
+  print(arr01);
+  print(obj01);
+}
