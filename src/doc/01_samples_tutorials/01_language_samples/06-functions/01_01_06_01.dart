50f1b0a2d9deadd5d33449ecd3c1e1852efb6400
luotao <luotaoyeah@gmail.com>
2019-06-14 10:57:19 +0800

    feat(doc): 01_01_06: functions

---
+++ src/doc/01_samples_tutorials/01_language_samples/06-functions/01_01_06_01.dart
@@ -0,0 +1,16 @@
+/*
+ * https://dart.dev/samples#functions
+ */
+
+void main() {
+  int result = fibonacci(5);
+  print(result);
+}
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
