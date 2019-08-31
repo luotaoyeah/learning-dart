6d8282ddfbb3e37e22ec1c67c6d5e312df8348c1
luotao <luotaoyeah@gmail.com>
2019-08-31 19:25:31 +0800

    feat(doc.language.language-tour): numbers

---
+++ src/doc/02-language/01-tour/03-built-in-types/01-numbers/02-01-03-01.01.dart
@@ -0,0 +1,31 @@
+/*
+ * https://dart.dev/guides/language/language-tour#numbers
+ */
+
+void main() {
+  fn01();
+  fn02();
+}
+
+void fn01() {
+  /*
+   * 数字类型有两种 int 和 double, 继承关系如下:
+   *   num
+   *     int
+   *     double
+   */
+
+  int i01 = 0;
+  double pi = 3.14;
+
+  num n01 = 9;
+  n01 = 3.14;
+}
+
+void fn02() {
+  int i01 = 9;
+  assert(i01.toString() == '9');
+
+  String s01 = '3.14';
+  assert(double.parse(s01) == 3.14);
+}
