bcda4c659aae4f88863606395880cfb29572db2a
luotao <luotaoyeah@gmail.com>
2019-09-02 18:19:35 +0800

    feat(doc.language.language-tour): bitwise-and-shift-operators

---
+++ src/doc/02-language/01-tour/05-operators/05-logical-operators/02-01-05-05.dart
@@ -0,0 +1,9 @@
+/*
+ * https://dart.dev/guides/language/language-tour#logical-operators
+ */
+
+void main() {
+  fn01();
+}
+
+fn01() {}
---
+++ src/doc/02-language/01-tour/05-operators/06-bitwise-and-shift-operators/02-01-05-06.dart
@@ -0,0 +1,9 @@
+/*
+ * https://dart.dev/guides/language/language-tour#bitwise-and-shift-operators
+ */
+
+void main() {
+  fn01();
+}
+
+fn01() {}
