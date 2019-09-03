9fe6755e7a72b87090d58e6dddb5a07f7949fc5d
luotao <luotaoyeah@gmail.com>
2019-09-03 17:24:55 +0800

    feat(doc.language.language-tour): other-operators

---
+++ src/doc/02-language/01-tour/05-operators/09-other-operators/02-01-05-09.dart
@@ -0,0 +1,26 @@
+/*
+ * https://dart.dev/guides/language/language-tour#other-operators
+ */
+
+void main() {
+  fn01();
+}
+
+/*
+ * ?. 跟 . 的区别在于:
+ *   如果 x.y 中的 x 为 null, 则会报错,
+ *   如果 x?.y 中的 x 为 null, 不会报错, 结果为 null
+ */
+void fn01() {
+  var v01 = null;
+
+  assert(v01?.x() == null);
+
+  var err;
+  try {
+    v01.x();
+  } catch (e) {
+    err = e;
+  }
+  assert(err is NoSuchMethodError);
+}
