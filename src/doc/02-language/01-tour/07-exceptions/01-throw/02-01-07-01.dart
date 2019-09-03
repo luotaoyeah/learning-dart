9ec0bc6b8a60b00ee014f20c6eccadb2641318bb
luotao <luotaoyeah@gmail.com>
2019-09-03 18:31:24 +0800

    feat(doc.language.language-tour): throw

---
+++ src/doc/02-language/01-tour/07-exceptions/01-throw/02-01-07-01.dart
@@ -0,0 +1,40 @@
+/*
+ * https://dart.dev/guides/language/language-tour#throw
+ */
+
+void main() {
+  fn01();
+  fn02();
+}
+
+/*
+ * dart 中的 exception 都是 unchecked exception,
+ * 即不需要在方法签名中声明该方法可能会抛出的异常
+ */
+void fn01() {
+  /*
+   * 异常/错误的基类有两个, Exception/Error
+   */
+
+  int i01 = 0;
+
+  if (i01 == 1) {
+    throw Error();
+  } else if (i01 == 2) {
+    throw Exception();
+  }
+}
+
+void fn02() {
+  /*
+   * throw 可以抛出任意类型的对象, 不一定是 Exception/Error 类型的对象
+   */
+
+  int i01 = 0;
+
+  if (i01 == 1) {
+    throw '1';
+  } else if (i01 == 2) {
+    throw 2;
+  }
+}
