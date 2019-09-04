2fac6a4069ba804ade1e2e174de6538402d229e3
luotao <luotaoyeah@gmail.com>
2019-09-04 18:55:09 +0800

    feat(doc.language.language-tour): invoking a non-default superclass constructor

---
+++ src/doc/02-language/01-tour/08-classes/05-constructors/02-01-08-05.04.dart
@@ -0,0 +1,46 @@
+/*
+ * https://dart.dev/guides/language/language-tour#constructors
+ */
+
+void main() {
+  fn01();
+}
+
+void fn01() {
+  var c01 = C02.fn01();
+  assert(c01.x == 'FOO');
+
+  var c02 = C02.fn02('BAR');
+  assert(c02.x == 'X');
+}
+
+class C01 {
+  String x;
+
+  C01(this.x);
+
+  C01.fn01(this.x) {
+    print('C01.fn01()');
+  }
+}
+
+/*
+ * 子类的 constructor 中必须调用父类的 constructor,
+ * 如果没有显式调用, 则会默认调用父类的 no-argument default constructor,
+ * 如果父类没有 no-argument default constructor, 则子类的 constructor 必须显式调用父类的某个 constructor
+ */
+class C02 extends C01 {
+  String y = 'Y';
+
+  C02.fn01() : super('FOO');
+
+  /*
+   * 代码的执行顺序如下:
+   *   1. initializer list
+   *   2. super-class constructor
+   *   3. sub-class constructor
+   */
+  C02.fn02(this.y) : super.fn01('X') {
+    print('C02.fn02()');
+  }
+}
