de2f2f9dbddd0653391d24d1f5a096a6dc7161b9
luotao <luotaoyeah@gmail.com>
2019-09-06 17:39:02 +0800

    feat(doc.language.language-tour): handling-futures

---
+++ src/doc/02-language/01-tour/11-asynchrony-support/01-handling-futures/02-01-11-01.01.dart
@@ -0,0 +1,19 @@
+/*
+ * https://dart.dev/guides/language/language-tour#handling-futures
+ */
+
+void main() {
+  fn01();
+}
+
+/*
+ * Future 类似于 JS 中的 Promise
+ */
+Future<int> fn01() {
+  return Future.delayed(Duration(seconds: 1), () => 9);
+}
+
+void fn02() async {
+  var i = await fn01();
+  assert(i == 9);
+}
