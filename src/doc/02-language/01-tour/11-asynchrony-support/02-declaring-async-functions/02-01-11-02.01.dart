878b92d248c03b6a45a0f2e365c9e4fbbdd69641
luotao <luotaoyeah@gmail.com>
2019-09-06 17:49:38 +0800

    feat(doc.language.language-tour): declaring-async-functions

---
+++ src/doc/02-language/01-tour/11-asynchrony-support/02-declaring-async-functions/02-01-11-02.01.dart
@@ -0,0 +1,26 @@
+/*
+ * https://dart.dev/guides/language/language-tour#declaring-async-functions
+ */
+
+void main() {
+  fn03().then((value) {
+    assert(value == 9);
+  });
+}
+
+/*
+ * async function 返回的是一个 `Future` 对象,
+ * 如果返回的值不是一个 `Future` 对象, dart 会自动将它包装成 `Future` 对象 
+ */
+
+Future<int> fn01() async {
+  return 9;
+}
+
+Future<int> fn02() async {
+  var future01 = fn01();
+  assert(future01 is Future<int>);
+  return future01;
+}
+
+Future<int> fn03() async => await fn02();
