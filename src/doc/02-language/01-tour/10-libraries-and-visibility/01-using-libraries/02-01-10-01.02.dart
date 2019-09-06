5b354c6a55f25543bcbaec64c75c87fd35901be9
luotao <luotaoyeah@gmail.com>
2019-09-06 17:12:23 +0800

    feat(doc.language.language-tour): specifying-a-library-prefix

---
+++ src/doc/02-language/01-tour/10-libraries-and-visibility/01-using-libraries/02-01-10-01.02.dart
@@ -0,0 +1,19 @@
+/*
+ * https://dart.dev/guides/language/language-tour#specifying-a-library-prefix
+ */
+
+/*
+ * 如果不同的 library 定义了同名的对象, 则可以给每个 library 取一个别名
+ */
+
+import './02-01-10-01.02.01.dart' as lib01;
+import './02-01-10-01.02.02.dart' as lib02;
+
+void main() {
+  fn01();
+}
+
+void fn01() {
+  assert(lib01.foo() == 9);
+  assert(lib02.foo() == 99);
+}
