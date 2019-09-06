57546d81625022d047ac85decb587ead46c52932
luotao <luotaoyeah@gmail.com>
2019-09-06 16:48:05 +0800

    feat(doc.language.language-tour): generic-collections-and-the-types-they-contain

---
+++ src/doc/02-language/01-tour/09-generics/04-generic-collections-and-the-types-they-contain/02-01-09-04.dart
@@ -0,0 +1,17 @@
+/*
+ * https://dart.dev/guides/language/language-tour#generic-collections-and-the-types-they-contain
+ */
+
+void main() {
+  fn01();
+}
+
+void fn01() {
+  /*
+   * 可以在运行时期查看对象的泛型类型
+   */
+
+  var list01 = List<int>();
+  assert(list01 is List<int>);
+  assert(list01.runtimeType.toString() == 'List<int>');
+}
