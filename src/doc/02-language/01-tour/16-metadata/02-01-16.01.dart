520810003c2f5a185235a10218721ac3c54e9d35
luotao <luotaoyeah@gmail.com>
2019-09-07 18:20:25 +0800

    feat(doc.language.language-tour): metadata

---
+++ src/doc/02-language/01-tour/16-metadata/02-01-16.01.dart
@@ -0,0 +1,26 @@
+/*
+ * https://dart.dev/guides/language/language-tour#metadata
+ */
+
+void main() {
+  fn01();
+}
+
+/*
+ * dart 中默认可以使用两个注解: `@override`, `@deprecated`
+ */
+
+class C01 {
+  fn01() {}
+}
+
+class C02 extends C01 {
+  @override
+  fn01() {
+    return super.fn01();
+  }
+}
+
+/// this function is deprecated
+@deprecated
+void fn01() {}
