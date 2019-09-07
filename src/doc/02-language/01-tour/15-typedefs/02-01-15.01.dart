52191006e1d21a7faa3e8fd4c545ef18d9b917ee
luotao <luotaoyeah@gmail.com>
2019-09-07 18:19:56 +0800

    feat(doc.language.language-tour): typedefs

---
+++ src/doc/02-language/01-tour/15-typedefs/02-01-15.01.dart
@@ -0,0 +1,31 @@
+/*
+ * https://dart.dev/guides/language/language-tour#typedefs
+ */
+
+void main() {
+  fn01();
+  fn02();
+}
+
+/*
+ * 可以给一个函数类型定义一个别名, 在其他地方就可以使用这个类型,
+ * 注意, 当前只支持给函数定义类型别名, 不支持给它其他的类型定义别名
+ */
+
+typedef Function01 = int Function(String x, int y);
+
+void fn01() {
+  Function01 fn01 = (String x, int y) => x.length + y;
+  assert(fn01 is Function01);
+  assert(fn01 is Function);
+}
+
+/*
+ * 函数类型别名还支持泛型参数
+ */
+typedef Function02<T extends num> = T Function(T t01, T t02);
+
+void fn02() {
+  Function02<int> fn02 = (int x, int y) => x + y;
+  assert(fn02 is Function02<int>);
+}
