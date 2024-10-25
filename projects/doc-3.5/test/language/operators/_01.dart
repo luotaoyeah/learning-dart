762c9a9ef306b729e6149d08d35a0f1dd9b402f0
luotao <luotaoyeah@gmail.com>
2024-10-25 22:18:36 +0800

    feat(doc-3.5): https://dart.dev/language/operators#equality-and-relational-operators

--- projects/doc-3.5/test/language/operators/_01.dart
+++ projects/doc-3.5/test/language/operators/_01.dart
@@ -9,4 +9,14 @@ void main() {
     expect(5 ~/ 2 == 2, true);
     expect(5.8 ~/ 2 == 2, true);
   });
+
+  // https://dart.dev/language/operators#equality-and-relational-operators
+  test("02", () {
+    // 父类 Object 中, == 运算符默认比较的是两个变量是否是同一个对象,
+    // 在具体的其他类中, 通常可以覆写 == 运算符, 实现该类自己的比较逻辑, 比如 String 类就覆写了 == 运算符,
+    String s01 = 'A';
+    String s02 = 'A';
+    expect(s01 == s02, true);
+    expect(identical(s01, s02), true);
+  });
 }
