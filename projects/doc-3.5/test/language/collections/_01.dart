4be3cfcaede59cc208b3c59869816c1bd8fd75cc
luotao <luotaoyeah@gmail.com>
2024-10-26 22:14:06 +0800

    feat(doc-3.5): https://dart.dev/language/collections#control-flow-operators

--- projects/doc-3.5/test/language/collections/_01.dart
+++ projects/doc-3.5/test/language/collections/_01.dart
@@ -12,7 +12,7 @@ void main() {
   });
 
   // https://dart.dev/language/collections#maps
-  test("02", () {
+  test("03", () {
     var map01 = <int, String>{};
 
     // 通过下标访问 map 的某个元素,
@@ -21,4 +21,18 @@ void main() {
     // length 属性表示元素个数,
     expect(map01.length, 1);
   });
+
+  // https://dart.dev/language/collections#control-flow-operators
+  test(
+    '04',
+    () {
+      // 在 list literal 中使用 collection if 语法, 根据条件决定是否插入某个元素,
+      var list01 = [1, 2, if (true) 3, if (false) 4];
+      expect(list01.length, 3);
+
+      // 在 list literal 中使用 collection for 语法, 循环插入元素,
+      var list02 = [1, for (int i in list01) i];
+      expect(list02.toString(), '[1, 1, 2, 3]');
+    },
+  );
 }
